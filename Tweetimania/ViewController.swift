c2591069c0667f2fd720b2b267f5db17d38b83bc
lordchingiss <jackli2014@gmail.com>
2019-09-07 16:19:31 -0700

    Fixed Swifter embedding error

--- Tweetimania/ViewController.swift
+++ Tweetimania/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var sentimentLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    let swifter = Swifter(consumerKey: "tHuzwg23jp6z3VnfGegv5ZxEn", consumerSecret: "J3RFRLopARFBtC9iRL1NyAdcVxIo2ETcP8FM31F6hPApcjo9NS")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    @IBAction func predict(_ sender: UIButton) {
+        
+    }
+    
 }
 
